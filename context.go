412426cc72ad3e1e1a2737fa25ac494ce773c279
ICHINOSE Shogo <shogo82148@gmail.com>
2023-08-09 13:10:27 +0900

    introduce contextual log level

--- context.go
+++ context.go
@@ -14,7 +14,50 @@ func (key *ctxKey) String() string {
 	return key.name
 }
 
+// key is the context key for the attributes.
 var key = &ctxKey{"ctxslog"}
+
+type ctxLevelKey struct{ name string }
+
+func (key *ctxLevelKey) String() string {
+	return key.name
+}
+
+// levelKey is the context key for the minimum record level.
+var levelKey = &ctxLevelKey{"ctxslog.level"}
+
+var _ slog.Handler = (*wrapper)(nil)
+
+type wrapper struct {
+	parent slog.Handler
+}
+
+func (w *wrapper) Handle(ctx context.Context, record slog.Record) error {
+	attrs := contextAttrs(ctx)
+	newRecord := record.Clone()
+	attrs.addToRecord(&newRecord)
+	return w.parent.Handle(ctx, newRecord)
+}
+
+func (w *wrapper) Enabled(ctx context.Context, level slog.Level) bool {
+	minLevel, ok := contextLevel(ctx)
+	if !ok {
+		return w.parent.Enabled(ctx, level)
+	}
+	return level >= minLevel
+}
+
+func (w *wrapper) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &wrapper{
+		parent: w.parent.WithAttrs(attrs),
+	}
+}
+
+func (w *wrapper) WithGroup(name string) slog.Handler {
+	return &wrapper{
+		parent: w.parent.WithGroup(name),
+	}
+}
 
 type mergedAttrs struct {
 	parent *mergedAttrs
@@ -105,14 +148,20 @@ func (attrs *mergedAttrs) addToRecord(record *slog.Record) {
 // New returns a new slog.Handler that injects the attributes from the context.
 func New(parent slog.Handler) slog.Handler {
 	return &wrapper{
-		handler: inject,
-		parent:  parent,
+		parent: parent,
 	}
 }
 
-func inject(ctx context.Context, parent func(ctx context.Context, record slog.Record) error, record slog.Record) error {
-	attrs := contextAttrs(ctx)
-	newRecord := record.Clone()
-	attrs.addToRecord(&newRecord)
-	return parent(ctx, newRecord)
+// WithLevel configures the minimum record level that will be logged in the context.
+func WithLevel(ctx context.Context, level slog.Level) context.Context {
+	return context.WithValue(ctx, levelKey, level)
+}
+
+// contextLevel returns the minimum record level that will be logged in the context.
+func contextLevel(ctx context.Context) (slog.Level, bool) {
+	level := ctx.Value(levelKey)
+	if level == nil {
+		return slog.LevelInfo, false
+	}
+	return level.(slog.Level), true
 }
